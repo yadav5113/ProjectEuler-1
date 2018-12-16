5b3146d248ee3757f55e5a73c3403dbd0285449c
Subham Sagar Paira <42293958+subhamsagar524@users.noreply.github.com>
2018-12-16 14:32:49 +0530

    Update problem10.cpp

--- problem10.cpp
+++ problem10.cpp
@@ -7,7 +7,7 @@ long long prime(long long n)
 	if (n%2 == 0)
 		return 0;
 	for (long long i=3; i <= sqrt(n); i += 2)		//You can also use pow(n,0.5), but it'a take 2.2 sec to execute (approx)
-	{												//This will only take 0.95 sec to execute (approx)
+	{							//This will only take 0.95 sec to execute (approx)
 		if (n%i == 0)
 			return 0;
 	}
@@ -21,6 +21,6 @@ int main()
 		if (prime(i))
 			sum += i;
 	}
-	cout << sum + 2;								//2 was skipped or you may initialize sum with 2 instead of 0
+	cout << sum + 2;					//2 was skipped or you may initialize sum with 2 instead of 0
 	return 0;
 }
